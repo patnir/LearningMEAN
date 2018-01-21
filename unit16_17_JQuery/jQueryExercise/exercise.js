6a2c1e569e4e6d782840d9bdc9ba67b311204bee
Rahul Patni <rpatni@purdue.edu>
2018-01-21 16:09:56 -0500

    jquery unit

---
+++ unit16_17_JQuery/jQueryExercise/exercise.js
@@ -0,0 +1,7 @@
+$("div").css("background", "purple");
+$("div.highlight").css("width", "200px");
+$("div#third").css("border", "2px solid orange");
+//$("div:nth-of-type(1)").css("color", "pink")
+//$("div:first-of-type").css("color", "pink")
+//$("div:first").css("color", "red"); //slower than others
+$("div").first().css("color", "pink"); //slower than others
