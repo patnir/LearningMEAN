544db9caa29f91211498a9a204752b6cde5e6a89
Rahul Patni <rpatni@purdue.edu>
2018-01-23 23:16:07 -0500

    adding persistances and consolidating notes

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -1,10 +1,35 @@
-var express = require("express");
-var app = express();
-var bodyparser = require("body-parser");
+var express     = require("express");
+var app         = express();
+var bodyparser  = require("body-parser");
+var mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+// Schema setup
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("campground", campgroundSchema);
+
+// Campground.create({
+//     name: "CG2", 
+//     image: "https://images.unsplash.com/photo-1486692957922-ea51ea8472bc?auto=format&fit=crop&w=1073&q=80"
+// }, function(err, campground) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log("CREATED CG1");
+//         console.log(campground);
+//     }
+// });
 
 var campgrounds = [
     {name: "CG1", image: "https://images.unsplash.com/photo-1439946612398-57d8d0ac915d?ixlib=rb-0.3.5&s=4005af16c32eb614cb5537b43b45cd7b&auto=format&fit=crop&w=1050&q=80"},
@@ -18,7 +43,16 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds) {
+        if(err) {
+            console.log("Error getting campgrounds " + err);
+        }
+        else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+
+   // res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.post("/campgrounds", function(req, res) {
@@ -28,8 +62,17 @@ app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newcamp = {name: name, image: image};
-    campgrounds.push(newcamp);
-    res.redirect("/campgrounds")
+    //campgrounds.push(newcamp);
+
+    Campground.create(newcamp, function(err, newCamp) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("CREATED campground");
+            res.redirect("/campgrounds")
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
